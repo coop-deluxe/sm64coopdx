fadea702fffc17b937f1faa768ddb33e6ccb890f
MysterD <myster@d>
2020-10-08 23:07:32 -0700

    Synchronized THI top

--- src/game/behaviors/thi_top.inc.c
+++ src/game/behaviors/thi_top.inc.c
@@ -16,15 +16,28 @@ void bhv_thi_huge_island_top_loop(void) {
 }
 
 void bhv_thi_tiny_island_top_loop(void) {
+    if (!network_sync_object_initialized(o)) {
+        network_init_object(o, SYNC_DISTANCE_ONLY_EVENTS);
+        network_init_object_field(o, &o->oAction);
+        network_init_object_field(o, &o->oPrevAction);
+        network_init_object_field(o, &o->oTimer);
+        network_init_object_field(o, &o->header.gfx.node.flags);
+    }
+
+    struct MarioState* marioState = nearest_mario_state_to_object(o);
+    struct Object* player = marioState->marioObj;
+    int distanceToPlayer = dist_between_objects(o, player);
+
     if (!(gTHIWaterDrained & 1)) {
         if (o->oAction == 0) {
-            if (o->oDistanceToMario < 500.0f)
-                if (gMarioStates->action == ACT_GROUND_POUND_LAND) {
+            if (distanceToPlayer < 500.0f)
+                if (marioState->action == ACT_GROUND_POUND_LAND) {
                     o->oAction++;
                     cur_obj_spawn_particles(&D_8032F134);
                     spawn_triangle_break_particles(20, 138, 0.3f, 3);
                     cur_obj_play_sound_2(SOUND_GENERAL_ACTIVATE_CAP_SWITCH);
                     cur_obj_hide();
+                    network_send_object(o);
                 }
         } else {
             if (o->oTimer < 50) {
