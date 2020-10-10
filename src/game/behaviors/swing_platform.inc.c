736fba54a2b71dd2fe37a6952a9bda6acc0993b8
MysterD <myster@d>
2020-10-10 15:32:39 -0700

    Synchronized swing platform

--- src/game/behaviors/swing_platform.inc.c
+++ src/game/behaviors/swing_platform.inc.c
@@ -1,9 +1,17 @@
 
 void bhv_swing_platform_init(void) {
     o->oSwingPlatformAngle = 0x2000;
+
+    struct SyncObject* so = network_init_object(o, 4000.0f);
+    so->minUpdateRate = 5.0f;
+    network_init_object_field(o, &o->oSwingPlatformAngle);
+    network_init_object_field(o, &o->oFaceAngleRoll);
+    network_init_object_field(o, &o->oSwingPlatformSpeed);
+    network_init_object_field(o, &o->oAngleVelRoll);
 }
 
 void bhv_swing_platform_update(void) {
+
     s32 startRoll = o->oFaceAngleRoll;
 
     if (o->oFaceAngleRoll < 0) {
