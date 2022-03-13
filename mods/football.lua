aded0b6d256f67f9967549342b29c65366fc34d3
mjcox244 <66311016+mjcox244@users.noreply.github.com>
2022-03-13 00:05:19 +0000

    Make Football a gamemode (#21)
    Small change to football to mark it as a game mode so you can't play H&S and football at once

--- mods/football.lua
+++ mods/football.lua
@@ -1,5 +1,6 @@
 -- name: Football (soccer)
 -- description: Play football in the castle grounds.\nOnly use a save that has lowered the water in the moat.
+-- incompatible: gamemode
 
 ---------------
 -- constants --
