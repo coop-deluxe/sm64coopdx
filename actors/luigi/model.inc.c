8d10529061095900f5bc50eeb1a4eedf92b3d943
MysterD <myster@d>
2022-04-16 23:53:10 -0700

    Fix Luigi metal color

--- actors/luigi/model.inc.c
+++ actors/luigi/model.inc.c
@@ -268,11 +268,11 @@ const Gfx luigi_butt[] = {
 const Gfx luigi_metal_butt[] = {
     gsDPPipeSync(),
     gsSPSetGeometryMode(G_TEXTURE_GEN),
-    gsDPSetCombineMode(G_CC_DECALFADE, G_CC_DECALFADE),
+    gsDPSetCombineLERP(TEXEL0, 0, SHADE, 0, 0, 0, 0, ENVIRONMENT, TEXEL0, 0, SHADE, 0, 0, 0, 0, ENVIRONMENT),
     gsDPLoadTextureBlock(luigi_texture_metal, G_IM_FMT_RGBA, G_IM_SIZ_16b, 64, 32, 0, G_TX_WRAP | G_TX_NOMIRROR, G_TX_WRAP | G_TX_NOMIRROR, 6, 5, G_TX_NOLOD, G_TX_NOLOD),
     gsSPTexture(0x0F80, 0x07C0, 0, G_TX_RENDERTILE, G_ON),
-    gsSPLight(&luigi_white_lights_group.l, 1),
-    gsSPLight(&luigi_white_lights_group.a, 2),
+    gsSPCopyLightEXT(1, 5),
+	gsSPCopyLightEXT(2, 6),
     gsSPDisplayList(luigi_butt_dl),
     gsSPEndDisplayList(),
 };
@@ -830,11 +830,11 @@ const Gfx luigi_left_thigh[] = {
 const Gfx luigi_metal_left_thigh[] = {
     gsDPPipeSync(),
     gsSPSetGeometryMode(G_TEXTURE_GEN),
-    gsDPSetCombineMode(G_CC_DECALFADE, G_CC_DECALFADE),
+    gsDPSetCombineLERP(TEXEL0, 0, SHADE, 0, 0, 0, 0, ENVIRONMENT, TEXEL0, 0, SHADE, 0, 0, 0, 0, ENVIRONMENT),
     gsDPLoadTextureBlock(luigi_texture_metal, G_IM_FMT_RGBA, G_IM_SIZ_16b, 64, 32, 0, G_TX_WRAP | G_TX_NOMIRROR, G_TX_WRAP | G_TX_NOMIRROR, 6, 5, G_TX_NOLOD, G_TX_NOLOD),
     gsSPTexture(0x0F80, 0x07C0, 0, G_TX_RENDERTILE, G_ON),
-    gsSPLight(&luigi_white_lights_group.l, 1),
-    gsSPLight(&luigi_white_lights_group.a, 2),
+    gsSPCopyLightEXT(1, 5),
+	gsSPCopyLightEXT(2, 6),
     gsSPDisplayList(luigi_left_thigh_dl),
     gsSPEndDisplayList(),
 };
