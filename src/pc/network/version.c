5983285e4be9afb2465210ef01b20e35d3d7aa2d
MysterD <myster@d>
2022-04-23 15:55:26 -0700

    Created a minor version number

--- src/pc/network/version.c
+++ src/pc/network/version.c
@@ -10,6 +10,6 @@ static char sVersionString[MAX_VERSION_LENGTH] = { 0 };
 #endif
 
 char* get_version(void) {
-    snprintf(sVersionString, MAX_VERSION_LENGTH, "%s%d", VERSION_TEXT, VERSION_NUMBER);
+    snprintf(sVersionString, MAX_VERSION_LENGTH, "%s%d.%d", VERSION_TEXT, VERSION_NUMBER, MINOR_VERSION_NUMBER);
     return sVersionString;
 }
--- src/pc/network/version.h
+++ src/pc/network/version.h
@@ -3,6 +3,7 @@
 
 #define UNSTABLE_BRANCH
 #define VERSION_NUMBER 25
+#define MINOR_VERSION_NUMBER 1
 
 #define MAX_VERSION_LENGTH 10
 char* get_version(void);
