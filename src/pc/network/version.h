8f35305d33af83daa5f762256bcda9c583aeecd7
MysterD <myster@d>
2022-04-29 22:13:16 -0700

    Bump version

--- src/pc/network/version.h
+++ src/pc/network/version.h
@@ -2,8 +2,8 @@
 #define VERSION_H
 
 #define UNSTABLE_BRANCH
-#define VERSION_NUMBER 25
-#define MINOR_VERSION_NUMBER 1
+#define VERSION_NUMBER 26
+#define MINOR_VERSION_NUMBER 0
 
 #define MAX_VERSION_LENGTH 10
 char* get_version(void);
