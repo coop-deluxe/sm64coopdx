ac8b03aaf297de91c6b12be7f24196982f02f44f
MysterD <myster@d>
2022-05-08 16:43:09 -0700

    The death of the unstable branch

--- src/pc/network/version.c
+++ src/pc/network/version.c
@@ -3,11 +3,7 @@
 #include "types.h"
 
 static char sVersionString[MAX_VERSION_LENGTH] = { 0 };
-#ifdef UNSTABLE_BRANCH
-#define VERSION_TEXT "unst "
-#else
 #define VERSION_TEXT "beta "
-#endif
 
 char* get_version(void) {
     snprintf(sVersionString, MAX_VERSION_LENGTH, "%s%d.%d", VERSION_TEXT, VERSION_NUMBER, MINOR_VERSION_NUMBER);
