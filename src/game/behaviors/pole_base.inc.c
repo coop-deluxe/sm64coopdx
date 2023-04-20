2f59d29f95174c7acb207ef6643b1ca0e9422192
MysterD <myster@d>
2023-04-20 00:26:17 -0700

    Fix crash in pole_base_loop()

--- src/game/behaviors/pole_base.inc.c
+++ src/game/behaviors/pole_base.inc.c
@@ -1,6 +1,7 @@
 // pole_base.inc.c
 
 void bhv_pole_base_loop(void) {
+    if (!gMarioObject) { return; }
     if (o->oPosY - 10.0f < gMarioObject->oPosY
         && gMarioObject->oPosY < o->oPosY + o->hitboxHeight + 30.0f)
         if (o->oTimer > 10)
