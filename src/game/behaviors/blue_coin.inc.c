75861d365e6ac3cb375c1825185d90fb9d5ccded
MysterD <myster@d>
2023-05-19 01:37:52 -0700

    Add backwards compatibility to ground pound checks

--- src/game/behaviors/blue_coin.inc.c
+++ src/game/behaviors/blue_coin.inc.c
@@ -136,7 +136,7 @@ void bhv_blue_coin_switch_loop(void) {
             // If Mario is on the switch and has ground-pounded,
             // recede and get ready to start ticking.
             if (gMarioObject && gMarioObject->platform == o) {
-                if (determine_interaction(&gMarioStates[0], o) & INT_GROUND_POUND) {
+                if ((determine_interaction(&gMarioStates[0], o) & INT_GROUND_POUND) || (gMarioStates[0].action == ACT_GROUND_POUND_LAND)) {
                     // Set to BLUE_COIN_SWITCH_ACT_RECEDING
                     o->oAction++;
 
