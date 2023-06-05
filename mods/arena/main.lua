60db46a6fa7bece81219941c0935a2a174e86f01
MysterD <myster@d>
2023-06-04 20:32:35 -0700

    Added spire level to arena

--- mods/arena/main.lua
+++ mods/arena/main.lua
@@ -28,6 +28,7 @@ LEVEL_ARENA_SKY_BEACH = level_register('level_arena_sky_beach_entry', COURSE_NON
 LEVEL_ARENA_PILLARS   = level_register('level_arena_pillars_entry',   COURSE_NONE, 'Pillars',   'pillars',   28000, 0x28, 0x28, 0x28)
 LEVEL_ARENA_FORTS     = level_register('level_arena_forts_entry',     COURSE_NONE, 'Forts',     'forts',     28000, 0x28, 0x28, 0x28)
 LEVEL_ARENA_CITADEL   = level_register('level_arena_citadel_entry',   COURSE_NONE, 'Citadel',   'citadel',   28000, 0x28, 0x28, 0x28)
+LEVEL_ARENA_SPIRE     = level_register('level_arena_spire_entry',     COURSE_NONE, 'Spire',     'spire',     28000, 0x28, 0x28, 0x28)
 
 local gGameLevels = {
     { level = LEVEL_ARENA_ORIGIN,    name = 'Origin'    },
@@ -35,6 +36,7 @@ local gGameLevels = {
     { level = LEVEL_ARENA_PILLARS,   name = 'Pillars'   },
     { level = LEVEL_ARENA_FORTS,     name = 'Forts'     },
     { level = LEVEL_ARENA_CITADEL,   name = 'Citadel' },
+    { level = LEVEL_ARENA_SPIRE,     name = 'Spire' },
 }
 
 -- expose certain functions to other mods
