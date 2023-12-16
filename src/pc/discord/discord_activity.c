949a05fd14dd1dfeb4f3ed143a3a3ea8ddc06042
Agent X <44549182+Agent-11@users.noreply.github.com>
2023-12-16 10:42:54 -0500

    Whoops lol

--- src/pc/discord/discord_activity.c
+++ src/pc/discord/discord_activity.c
@@ -68,10 +68,7 @@ static void strncat_len(char* destination, char* source, size_t destinationLengt
 
 static void discord_populate_details(char* buffer, int bufferLength) {
     // get version
-    bool stored = gCoopCompatibility;
-    gCoopCompatibility = true;
     const char* version = get_version();
-    gCoopCompatibility = stored;
     int versionLength = strlen(version);
     snprintf(buffer, bufferLength, "%s", version);
     buffer += versionLength;
