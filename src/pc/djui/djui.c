9cfe26bdbe62dd48129e42b894bfa745baef4270
Agent X <44549182+Agent-11@users.noreply.github.com>
2024-01-01 16:19:10 -0500

    Update changelog

--- src/pc/djui/djui.c
+++ src/pc/djui/djui.c
@@ -97,7 +97,6 @@ void djui_init_late(void) {
         }
         if (strcmp(configLastVersion, SM64COOPDX_VERSION)) {
             djui_panel_changelog_create(NULL);
-            strncpy(configLastVersion, SM64COOPDX_VERSION, MAX_CONFIG_STRING);
         }
         //djui_panel_debug_create();
     }
--- src/pc/djui/djui_panel_changelog.c
+++ src/pc/djui/djui_panel_changelog.c
@@ -2,40 +2,56 @@
 #include "djui_panel.h"
 #include "djui_panel_menu.h"
 
-static char sChangelog[1024];
+static char sChangelog[1200];
+
+void djui_panel_changelog_back(UNUSED struct DjuiBase* caller) {
+    strncpy(configLastVersion, SM64COOPDX_VERSION, MAX_CONFIG_STRING);
+    djui_panel_back();
+}
 
 void djui_panel_changelog_create(struct DjuiBase* caller) {
     struct DjuiThreePanel* panel = djui_panel_menu_create(DLANG(CHANGELOG, CHANGELOG_TITLE));
     struct DjuiBase* body = djui_three_panel_get_body(panel);
 
     {
-        snprintf(sChangelog, 1024, "Welcome back! - v0.1.2:\n\
+        snprintf(sChangelog, 1200, "Welcome back! - v0.1.3:\n\
 Changes:\n\
-Disable sm64ex-coop Compatibility by default\n\
-Restored Luigi's original color palette\n\
-Made mario step code use his hitbox height instead of a constant number for his height\n\
-Hid Mario's shadow in First Person\n\
-Cancelled First Person if Mario is flying\n\
-Updated language files\n\
-Add --console launch parameter\n\
+Made Lua error logging more readable\n\
+Made GitHub README clearer\n\
+Made capitalization of some menu strings consistent in English.ini\n\
+Removed the need to restart to change sm64ex-coop compatibility\n\
+Improved Discord rich presence by adding images\n\
+Re-added credits camera movement easter egg but with the player 1 controller this time\n\
+Made default network player ping 50 again from 600\n\
+Made player text color base off of CAP color instead of SHIRT color\n\
 Fixes:\n\
-Reverted dialog cutscene changes to prevent softlocks\n\
-Fixed DynOS related crashes\n\
-Fixed Mario's shadow not resetting\n\
+Fixed incompatibility tags only registering the first one\n\
+Fixed linker errors with macOS\n\
+Fixed corrupted mod levels showing up on the main menu\n\
+Fixed crash with disconnect key on star select screen\n\
+Fixed singleplayer not forcing direct connection\n\
+Fixed water camera and TTM star cutscene in first person\n\
+Fixed random crashes with built-in mods\n\
+Lua API Changes:\n\
+Allowed luac files to be read\n\
+Allowed over exposure (>255) in marioBodyState light and shade values\n\
+Undefined SM64COOPDX_VERSION if sm64ex-coop compatibility is on\n\
+Added get_coop_compatibility_enabled()\n\
+Added gControllers\n\
 Mod Changes:\n\
-Updated Gun Mod DX, Day Night Cycle, Character Select and mQuake\n\
-Updated SiM description\n\
-Removed Flood and Cheats due to future plans with them"
+Fixed custom music bug and updated rainbow level with Arena\n\
+Updated mQuake\n\
+Re-added Hide and Seek"
         );
 
         struct DjuiText* text = djui_text_create(body, sChangelog);
         djui_base_set_location(&text->base, 0, 0);
-        djui_base_set_size(&text->base, (DJUI_DEFAULT_PANEL_WIDTH * (configDjuiThemeCenter ? DJUI_THEME_CENTERED_WIDTH : 1)) - 64, 497);
+        djui_base_set_size(&text->base, (DJUI_DEFAULT_PANEL_WIDTH * (configDjuiThemeCenter ? DJUI_THEME_CENTERED_WIDTH : 1)) - 64, 760);
         djui_base_set_color(&text->base, 220, 220, 220, 255);
         djui_text_set_drop_shadow(text, 64, 64, 64, 100);
         djui_text_set_alignment(text, DJUI_HALIGN_LEFT, DJUI_VALIGN_TOP);
 
-        djui_button_create(body, "OK", DJUI_BUTTON_STYLE_BACK, djui_panel_menu_back);
+        djui_button_create(body, "OK", DJUI_BUTTON_STYLE_BACK, djui_panel_changelog_back);
     }
 
     djui_panel_add(caller, panel, NULL);
