65e913034f240acf59e20e2f904335bcc402e147
Agent X <44549182+Agent-11@users.noreply.github.com>
2024-03-02 17:15:49 -0500

    Coop compatible object pool capacity increase
    Nvm it was fine with coop all along I'm just a dumbass.

--- src/game/object_list_processor.h
+++ src/game/object_list_processor.h
@@ -23,7 +23,7 @@
 /**
  * The maximum number of objects that can be loaded at once.
  */
-#define OBJECT_POOL_CAPACITY 960
+#define OBJECT_POOL_CAPACITY 1200
 
 /**
  * Every object is categorized into an object list, which controls the order
