8c6102d1e13e310bd64f71eb73b428daeb2e1102
Agent X <44549182+Agent-11@users.noreply.github.com>
2024-03-05 21:14:01 -0500

    Make dirt particle shadable

--- actors/dirt/model.inc.c
+++ actors/dirt/model.inc.c
@@ -49,7 +49,7 @@ const Gfx dirt_seg3_dl_0302BFF8[] = {
 // 0x0302C028 - 0x0302C098
 const Gfx dirt_seg3_dl_0302C028[] = {
     gsDPPipeSync(),
-    gsDPSetCombineMode(G_CC_DECALRGBA, G_CC_DECALRGBA),
+    gsDPSetCombineMode(G_CC_MODULATERGBA, G_CC_MODULATERGBA),
     gsSPClearGeometryMode(G_CULL_BACK),
     gsDPSetTile(G_IM_FMT_RGBA, G_IM_SIZ_16b, 0, 0, G_TX_LOADTILE, 0, G_TX_WRAP | G_TX_NOMIRROR, G_TX_NOMASK, G_TX_NOLOD, G_TX_WRAP | G_TX_NOMIRROR, G_TX_NOMASK, G_TX_NOLOD),
     gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
