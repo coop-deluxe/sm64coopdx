5eca2fc291c666b6d80e549272d59be9415e6171
Agent X <44549182+Agent-11@users.noreply.github.com>
2024-03-24 20:11:18 -0400

    Fix main panel on lower resolutions

--- src/pc/djui/djui_panel_main.c
+++ src/pc/djui/djui_panel_main.c
@@ -30,21 +30,21 @@ void djui_panel_main_create(struct DjuiBase* caller) {
         struct DjuiBase* body = djui_three_panel_get_body(panel);
         {
             struct DjuiImage* logo = djui_image_create(body, texture_coopdx_logo, 2048, 1024, 32);
-            djui_base_set_size(&logo->base, 500, 250);
+            djui_base_set_size(&logo->base, 400, 200);
             djui_base_set_alignment(&logo->base, DJUI_HALIGN_CENTER, DJUI_VALIGN_TOP);
             djui_base_set_location_type(&logo->base, DJUI_SVT_RELATIVE, DJUI_SVT_ABSOLUTE);
-            djui_base_set_location(&logo->base, 0, -70);
+            djui_base_set_location(&logo->base, 0, -30);
 
             struct DjuiButton* button1 = djui_button_create(body, DLANG(MAIN, HOST), DJUI_BUTTON_STYLE_NORMAL, djui_panel_host_create);
-            djui_base_set_location(&button1->base, 0, -60);
+            djui_base_set_location(&button1->base, 0, -30);
             djui_cursor_input_controlled_center(&button1->base);
 
             struct DjuiButton* button2 = djui_button_create(body, DLANG(MAIN, JOIN), DJUI_BUTTON_STYLE_NORMAL, djui_panel_join_create);
-            djui_base_set_location(&button2->base, 0, -60);
+            djui_base_set_location(&button2->base, 0, -30);
             struct DjuiButton* button3 = djui_button_create(body, DLANG(MAIN, OPTIONS), DJUI_BUTTON_STYLE_NORMAL, djui_panel_options_create);
-            djui_base_set_location(&button3->base, 0, -60);
+            djui_base_set_location(&button3->base, 0, -30);
             struct DjuiButton* button4 = djui_button_create(body, DLANG(MAIN, QUIT), DJUI_BUTTON_STYLE_BACK, djui_panel_main_quit);
-            djui_base_set_location(&button4->base, 0, -60);
+            djui_base_set_location(&button4->base, 0, -30);
         }
 
         // these two cannot co-exist for some reason
