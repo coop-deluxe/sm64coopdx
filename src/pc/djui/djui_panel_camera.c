3c6c11b08a47e8867f143ce847126125b64840e7
Agent X <44549182+Agent-11@users.noreply.github.com>
2024-03-28 21:54:54 -0400

    Restore camera pan level slider

--- src/pc/djui/djui_panel_camera.c
+++ src/pc/djui/djui_panel_camera.c
@@ -21,6 +21,7 @@ void djui_panel_camera_create(struct DjuiBase* caller) {
         djui_slider_create(body, DLANG(CAMERA, X_SENSITIVITY), &configCameraXSens, 1, 100, djui_panel_camera_value_changed);
         djui_slider_create(body, DLANG(CAMERA, Y_SENSITIVITY), &configCameraYSens, 1, 100, djui_panel_camera_value_changed);
         djui_slider_create(body, DLANG(CAMERA, AGGRESSION), &configCameraAggr, 0, 100, djui_panel_camera_value_changed);
+        djui_slider_create(body, DLANG(CAMERA, PAN_LEVEL), &configCameraPan, 0, 100, djui_panel_camera_value_changed);
         djui_slider_create(body, DLANG(CAMERA, DECELERATION), &configCameraDegrade, 0, 100, djui_panel_camera_value_changed);
 
         djui_button_create(body, DLANG(MENU, BACK), DJUI_BUTTON_STYLE_BACK, djui_panel_menu_back);
