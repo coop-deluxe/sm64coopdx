95677cc5e9f611e5a2b55d595beaced38f98e636
Agent X <44549182+AgentXLP@users.noreply.github.com>
2024-06-28 15:48:50 -0400

    "Fix"

--- src/game/object_helpers.c
+++ src/game/object_helpers.c
@@ -1951,7 +1951,7 @@ void obj_set_cylboard(struct Object *obj) {
 
 void cur_obj_set_billboard_if_vanilla_cam(void) {
     if (!o) { return; }
-    if (camera_config_is_free_cam_enabled() || get_first_person_enabled()) {
+    if (get_first_person_enabled()) {
         o->header.gfx.node.flags &= ~GRAPH_RENDER_BILLBOARD;
         o->header.gfx.node.flags |= GRAPH_RENDER_CYLBOARD;
     } else {
