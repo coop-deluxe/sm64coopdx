c144ff678002d413ea2e3bf77cf7683c9a41f646
Agent X <44549182+AgentXLP@users.noreply.github.com>
2024-06-30 17:46:18 -0400

    Remove unused Waluigi animation

--- autogen/lua_definitions/constants.lua
+++ autogen/lua_definitions/constants.lua
@@ -5500,9 +5500,6 @@ MARIO_ANIM_TRIPLE_JUMP_FLY = 208
 --- @type MarioAnimID
 WALUIGI_ANIM_WALKING = 209
 
---- @type MarioAnimID
-WALUIGI_ANIM_RUNNING = 210
-
 --- @class MarioCapGSCId
 
 --- @type MarioCapGSCId
