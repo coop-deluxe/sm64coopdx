b03237f24f818e0de5d7e1fa3e7caac1cc89896f
Agent X <44549182+AgentXLP@users.noreply.github.com>
2024-07-01 12:13:34 -0400

    Revert "Small crash fix"
    This reverts commit 95738ccfd157ee2c290efda9d7ec7e91c8844c52.

--- src/pc/djui/djui_panel_player.c
+++ src/pc/djui/djui_panel_player.c
@@ -458,7 +458,5 @@ void djui_panel_player_create(struct DjuiBase* caller) {
     }
 
     struct DjuiPanel* p = djui_panel_add(caller, panel, NULL);
-    if (p != NULL) {
-        p->on_panel_destroy = djui_panel_player_destroy;
-    }
+    p->on_panel_destroy = djui_panel_player_destroy;
 }
