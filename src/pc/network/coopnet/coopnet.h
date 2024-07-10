ba8a82724e06679e16e831e8a8eb19db5eeec5aa
Emerald Lockdown <calebelm@icloud.com>
2024-07-09 21:39:23 -0500

    move desc length define to top

--- src/pc/network/coopnet/coopnet.h
+++ src/pc/network/coopnet/coopnet.h
@@ -2,14 +2,14 @@
 #define COOPNET_H
 #ifdef COOPNET
 
+#define MAX_COOPNET_DESCRIPTION_LENGTH 1024
+
 typedef void (*QueryCallbackPtr)(uint64_t aLobbyId, uint64_t aOwnerId, uint16_t aConnections, uint16_t aMaxConnections, const char* aGame, const char* aVersion, const char* aHostName, const char* aMode, const char* aDescription);
 typedef void (*QueryFinishCallbackPtr)(void);
 
 extern struct NetworkSystem gNetworkSystemCoopNet;
 extern uint64_t gCoopNetDesiredLobby;
 extern char gCoopNetPassword[];
-
-#define MAX_COOPNET_DESCRIPTION_LENGTH 1024
 
 bool ns_coopnet_query(QueryCallbackPtr callback, QueryFinishCallbackPtr finishCallback, const char* password);
 bool ns_coopnet_is_connected(void);
