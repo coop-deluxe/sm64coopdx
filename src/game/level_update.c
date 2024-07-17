c153162fa9f6eeb447880ff2640f68ae6e59ff9f
Agent X <44549182+AgentXLP@users.noreply.github.com>
2024-07-17 13:55:41 -0400

    Disable demos for now, this whole main menu needs a rewrite

--- src/game/level_update.c
+++ src/game/level_update.c
@@ -1493,11 +1493,11 @@ void update_menu_level(void) {
     stop_cap_music();
     if (!configMenuSound || configMenuStaffRoll || curLevel == LEVEL_CASTLE_GROUNDS) {
         reset_volume();
-        sound_banks_disable(SEQ_PLAYER_SFX, SOUND_BANKS_BACKGROUND);
+        disable_background_sound();
         set_background_music(0, SEQ_MENU_TITLE_SCREEN, 0);
     } else {
         reset_volume();
-        sound_banks_disable(SEQ_PLAYER_SFX, SOUND_BANKS_BACKGROUND);
+        disable_background_sound();
         if (gCurrentArea != NULL) {
             set_background_music(gCurrentArea->musicParam, gCurrentArea->musicParam2, 0);
         }
