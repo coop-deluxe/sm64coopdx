1afeb6f87dbccb1b1449b868ee65877881e8e4fe
Sunk <69110309+Sunketchupm@users.noreply.github.com>
2024-10-08 20:47:03 -0400

    Mild kick knockback increase from nerf

--- src/game/interaction.c
+++ src/game/interaction.c
@@ -673,7 +673,7 @@ u32 determine_knockback_action(struct MarioState *m, UNUSED s32 arg) {
             if (m2->marioObj == NULL) { continue; }
             if (m2->marioObj != m->interactObj) { continue; }
             // Redundent check in case the kicking flag somehow gets missed
-            if (m2->action == ACT_JUMP_KICK || m2->flags & MARIO_KICKING) { scaler = 1.75f; }
+            if (m2->action == ACT_JUMP_KICK || m2->flags & MARIO_KICKING) { scaler = 1.85f; }
             else if (m2->action == ACT_DIVE) { scaler = 1 + fabs(m2->forwardVel * 0.01f); }
             else if ((m2->flags & MARIO_PUNCHING)) { scaler = 0.2f; hasBeenPunched = TRUE; }
             if (m2->flags & MARIO_METAL_CAP) { scaler *= 1.25f; }
