b012bd8fefd1f3152703f9d03d9af2d2f242cdff
John S <138552829+Multi-Volt@users.noreply.github.com>
2024-10-17 22:15:29 -0400

    Fixed the use of a custom sqrt approximation as modern procs have a built in FSQRT instruction that is faster.

--- src/pc/lua/utils/smlua_math_utils.c
+++ src/pc/lua/utils/smlua_math_utils.c
@@ -17,15 +17,8 @@ s16 degrees_to_sm64(f32 degreesAngle) {
     return degreesAngle * 0x8000 / 180.0f;
 }
 
-f32 fast_hypot(f32 a, f32 b) {
-    f32 sum = a * a + b * b;   // Fast sqrt derived from fast invsqrt
-    u32 i = *(u32 *) &sum;     // Bit-level manipulation of float
-    i = (i + 0x3f76cf62) >> 1; // Magic number and bit-shift
-    f32 result = *(f32 *) &i;
-    // Optional: Two iterations of Newton's method for better accuracy
-    result = 0.5f * (result + sum / result);
-    result = 0.5f * (result + sum / result);
-    return result;
+f32 hypotf(f32 a, f32 b) {
+    return sqrtf(a * a + b * b);
 }
 
 s32 clamp(s32 a, s32 b, s32 c) {
