fff34af6acf82471e773fbc01937cf18f040704c
Agent X <44549182+AgentXLP@users.noreply.github.com>
2024-11-12 19:01:02 -0500

    Disable file select on OG title screen (accessible with special_warp(SPECIAL_WARP_TITLE))

--- levels/intro/script.c
+++ levels/intro/script.c
@@ -63,7 +63,8 @@ const LevelScript level_intro_mario_head_regular[] = {
     TRANSITION(/*transType*/ WARP_TRANSITION_FADE_FROM_STAR, /*time*/ 20, /*color*/ 0x00, 0x00, 0x00),
     SLEEP(/*frames*/ 20),
     CALL_LOOP(/*arg*/ 1, /*func*/ lvl_intro_update),
-    JUMP_IF(/*op*/ OP_EQ, /*arg*/ 100, script_intro_L1),
+    // JUMP_IF(/*op*/ OP_EQ, /*arg*/ 100, script_intro_L1),
+    JUMP_IF(/*op*/ OP_EQ, /*arg*/ 100, script_intro_L4),
     JUMP_IF(/*op*/ OP_EQ, /*arg*/ 101, script_intro_L2),
     JUMP(script_intro_L4),
 };
@@ -88,7 +89,8 @@ const LevelScript level_intro_mario_head_dizzy[] = {
     TRANSITION(/*transType*/ WARP_TRANSITION_FADE_FROM_STAR, /*time*/ 20, /*color*/ 0x00, 0x00, 0x00),
     SLEEP(/*frames*/ 20),
     CALL_LOOP(/*arg*/ 2, /*func*/ lvl_intro_update),
-    JUMP_IF(/*op*/ OP_EQ, /*arg*/ 100, script_intro_L1),
+    // JUMP_IF(/*op*/ OP_EQ, /*arg*/ 100, script_intro_L1),
+    JUMP_IF(/*op*/ OP_EQ, /*arg*/ 100, script_intro_L4),
     JUMP_IF(/*op*/ OP_EQ, /*arg*/ 101, script_intro_L2),
     JUMP(script_intro_L4),
 };
