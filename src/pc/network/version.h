520d09c28064dccca20fb6c03945ef4410a32202
Agent X <44549182+AgentXLP@users.noreply.github.com>
2024-12-11 23:56:05 -0500

    New window title

--- src/pc/network/version.h
+++ src/pc/network/version.h
@@ -9,21 +9,24 @@
 #define MINOR_VERSION_NUMBER 0
 
 #if defined(VERSION_JP)
-    #define VERSION_REGION "JP"
+#define VERSION_REGION "JP"
 #elif defined(VERSION_EU)
-    #define VERSION_REGION "EU"
+#define VERSION_REGION "EU"
 #elif defined(VERSION_SH)
-    #define VERSION_REGION "SH"
+#define VERSION_REGION "SH"
 #else
-    #define VERSION_REGION "US"
+#define VERSION_REGION "US"
 #endif
 
 #ifdef DEVELOPMENT
-    #define GAME_NAME "sm64coopdx-dev"
+#define GAME_NAME   "sm64coopdx-dev"
+#define WINDOW_NAME "Super Mario 64 Coop Deluxe (DEV)"
 #elif !defined(VERSION_US)
-    #define GAME_NAME "sm64coopdx-intl"
+#define GAME_NAME "sm64coopdx-intl"
+#define WINDOW_NAME "Super Mario 64 Coop Deluxe (INTL)"
 #else
-    #define GAME_NAME "sm64coopdx"
+#define GAME_NAME "sm64coopdx"
+#define WINDOW_NAME "Super Mario 64 Coop Deluxe"
 #endif
 
 #define MAX_VERSION_LENGTH 32
