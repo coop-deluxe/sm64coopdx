3dd190413dbc4b8f65a775be0ee235847effbaac
Mr.Needlemouse <103795210+64BitMouse@users.noreply.github.com>
2024-12-17 17:31:40 -0500

    File Removal
    Removing Level folders and behaviordata.c via request

--- mods/star-road/levels/cotmc/areas/1/movtextNew.inc.c
+++
@@ -1,6 +0,0 @@
-static Movtex cotmc_1_Movtex_0_0[] = {1, 0, 15, 3, -8192, -8192, -8192, 8192, 8192, 8192, 8192, -8192, 1, 120, 0, 0};
-
-const struct MovtexQuadCollection cotmc_1_Movtex_0[] = {
-{0,cotmc_1_Movtex_0_0},
-{-1, NULL},
-};
