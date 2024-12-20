d6a946d884e0afc913008a3f0253f9cef395592f
Mike <jjelamb@gmail.com>
2024-12-19 21:03:57 -0600

    more common kanji

--- src/pc/djui/djui_language.c
+++ src/pc/djui/djui_language.c
@@ -36,7 +36,7 @@ char* djui_language_get(const char *section, const char *key) {
 
 char* djui_language_find_key(const char* section, const char* value) {
     if (!sLang) return NULL;
-    return ini_find_key(sLang, section, value);
+    return (char*)ini_find_key(sLang, section, value);
 }
 
 void djui_language_replace(char* src, char* dst, int size, char key, char* value) {
