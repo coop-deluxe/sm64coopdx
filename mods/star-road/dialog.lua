44e32f8bf253165a4cb3d9cbd2f12a52949c322e
Agent X <44549182+AgentXLP@users.noreply.github.com>
2024-12-24 18:32:50 -0500

    Star Road code overhaul

--- mods/star-road/dialog.lua
+++ mods/star-road/dialog.lua
@@ -1,8 +1,3 @@
-changed_dialogs = {}
-
-
-
-
 smlua_text_utils_dialog_replace(DIALOG_000, 1, 6, 30, 200, "--WARNING--\
 FLOOR UNSTABLE. DO NOT\
 GROUND-POUND UNDER \
@@ -1610,7 +1605,6 @@ their houses!")
 smlua_text_utils_dialog_replace(DIALOG_168, 1, 5, 30, 200, "Ack! Stop it or I'll\
 make you stop!")
 
-changed_dialogs[169] = 1
 smlua_text_utils_dialog_replace(DIALOG_169, 1, 6, 30, 200, "Psst! Looking for\
 a challenge? Try pressing\
 [R] after pausing the\
@@ -1621,14 +1615,11 @@ SMSR veterans, try holding\
 the L Button before you\
 host a lobby with 'Super\
 Mario Star Road' as the\
-active romhack! ")
-
-
+active romhack!")
 
 function dialog_replace()
       if gGlobalSyncTable.hardMode then
-            changed_dialogs[169] = 1 -- thank you Emmi for being so cool and having this in MarioHunt so I could remember how to change dialog this way
-smlua_text_utils_dialog_replace(DIALOG_169, 1, 4, 30, 200, "Welcome to HARD MODE!\
+            smlua_text_utils_dialog_replace(DIALOG_169, 1, 4, 30, 200, "Welcome to HARD MODE!\
 This challenge mode is an\
 OHKO, which means that\
 everything-- and I mean\
@@ -1637,8 +1628,7 @@ one hit! Do you have what\
 it takes to complete this\
 challenge?")
       else
-            changed_dialogs[169] = 1
-smlua_text_utils_dialog_replace(DIALOG_169, 1, 6, 30, 200, "Psst! Looking for\
+            smlua_text_utils_dialog_replace(DIALOG_169, 1, 6, 30, 200, "Psst! Looking for\
 a challenge? Try pressing\
 [R] after pausing the\
 game, and select the\
@@ -1648,10 +1638,8 @@ SMSR veterans, try holding\
 the L Button before you\
 host a lobby with 'Super\
 Mario Star Road' as the\
-active romhack! ")
+active romhack!")
       end
 end
-
-
 
 hook_event(HOOK_ON_DIALOG, dialog_replace)
