939218dcf91791be3be2e2525c1dc9ef54beaa83
Isaac0-dev <62234577+Isaac0-dev@users.noreply.github.com>
2025-01-06 09:07:16 +1000

    smlua improvements (#607)
    * smlua improvements
    
    * fix non dev compile error
    
    * fixes

--- src/pc/lua/smlua.c
+++ src/pc/lua/smlua.c
@@ -1,5 +1,4 @@
 #include "smlua.h"
-#include "smlua_cobject_map.h"
 #include "game/hardcoded.h"
 #include "pc/mods/mods.h"
 #include "pc/mods/mods_utils.h"
@@ -282,7 +281,6 @@ static void smlua_load_script(struct Mod* mod, struct ModFile* file, u16 remoteI
 
 void smlua_init(void) {
     smlua_shutdown();
-    smlua_pointer_user_data_init();
 
     gLuaState = luaL_newstate();
     lua_State* L = gLuaState;
@@ -362,7 +360,6 @@ void smlua_shutdown(void) {
     smlua_text_utils_reset_all();
     smlua_audio_utils_reset_all();
     audio_custom_shutdown();
-    smlua_pointer_user_data_shutdown();
     smlua_clear_hooks();
     smlua_model_util_clear();
     smlua_level_util_reset();
