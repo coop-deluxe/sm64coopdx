a5d0cd5f711a376e0b40b4b6c2335373602b5467
iZePlayzYT <izeplayzyt@gmail.com>
2025-01-18 08:03:05 +0100

    Added new logging system to create and write stuff into .log files.

---
+++ src/pc/logging.h
@@ -0,0 +1,49 @@
+#ifndef LOGGING_H
+#define LOGGING_H
+
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * Enum representing the different logging categories.
+ */
+typedef enum {
+    LOG_CATEGORY_RUNTIME,
+    LOG_CATEGORY_GAME,
+    LOG_CATEGORY_SETTINGS,
+    LOG_CATEGORY_MODS,
+    LOG_CATEGORY_NETWORK,
+    LOG_CATEGORY_CLIENT,
+    LOG_CATEGORY_SERVER,
+    LOG_CATEGORY_COOPNET,
+    LOG_CATEGORY_DEBUG
+} LogCategory;
+
+/**
+ * Enum representing the different log types.
+ */
+typedef enum {
+    LOG_TYPE_INFO,
+    LOG_TYPE_WARN,
+    LOG_TYPE_ERROR,
+    LOG_TYPE_CRASH,
+    LOG_TYPE_DEBUG
+} LogType;
+
+/**
+ * Logs a message with optional additional arguments.
+ * IMPORTANT: Always terminate the variadic arguments with NULL.
+ * If the NULL parameter is omitted, the program will crash.
+ * 
+ * Examples:
+ * log_message(LOG_CATEGORY_CLIENT, LOG_TYPE_INFO, "Test", NULL);
+ * log_message(LOG_CATEGORY_CLIENT, LOG_TYPE_INFO, "Hello", " World", "!", NULL);
+ *
+ * @param category The log category (e.g., LOG_CATEGORY_GAME).
+ * @param type The log type (e.g., LOG_TYPE_INFO).
+ * @param message The base message string.
+ * @param ... Additional strings to concatenate, terminated by NULL.
+ */
+void log_message(LogCategory category, LogType type, const char *message, ...);
+
+#endif
