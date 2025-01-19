75b71e9b4d4270493c5b722432b9f4fd7a6384bc
Isaac0-dev <62234577+Isaac0-dev@users.noreply.github.com>
2025-01-19 13:46:28 +1000

    fix nametags and collisions with mirror mario

--- src/game/object_list_processor.c
+++ src/game/object_list_processor.c
@@ -277,7 +277,7 @@ void bhv_mario_update(void) {
 
     // sanity check torsoPos, it isn't updated off-screen otherwise
     extern u32 gGlobalTimer;
-    if (gMarioState->marioBodyState && gMarioState->marioBodyState->updateTorsoTime != (gGlobalTimer - 1)) {
+    if (gMarioState->marioBodyState && !gMarioState->marioBodyState->mirrorMario && gMarioState->marioBodyState->updateTorsoTime != (gGlobalTimer - 1)) {
         vec3f_copy(gMarioState->marioBodyState->torsoPos, gMarioState->pos);
     }
 
