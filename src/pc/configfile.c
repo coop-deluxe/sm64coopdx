2fca847499d52f2560d1c8838760b0cf23175a3f
Isaac0-dev <62234577+Isaac0-dev@users.noreply.github.com>
2025-03-04 17:12:23 +1000

    bring back skip_pack_generation

--- src/pc/configfile.c
+++ src/pc/configfile.c
@@ -189,6 +189,7 @@ unsigned int configDjuiScale                      = 0;
 // other
 unsigned int configRulesVersion                   = 0;
 bool         configCompressOnStartup              = false;
+bool         configSkipPackGeneration             = false;
 
 // secrets
 bool configExCoopTheme = false;
@@ -320,6 +321,7 @@ static const struct ConfigOption options[] = {
     // other
     {.name = "rules_version",                  .type = CONFIG_TYPE_UINT,   .uintValue   = &configRulesVersion},
     {.name = "compress_on_startup",            .type = CONFIG_TYPE_BOOL,   .boolValue   = &configCompressOnStartup},
+    {.name = "skip_pack_generation",           .type = CONFIG_TYPE_BOOL,   .boolValue   = &configSkipPackGeneration},
 };
 
 struct SecretConfigOption {
