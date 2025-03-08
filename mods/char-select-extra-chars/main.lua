22726c58b40d120e699f8dccafab7b39d5a6d687
Yuyake <aaronkasarion@gmail.com>
2025-03-07 20:17:07 -0700

    Rosalina, Movesets and Updated Visuals

--- mods/char-select-extra-chars/main.lua
+++ mods/char-select-extra-chars/main.lua
@@ -1,5 +1,7 @@
--- name: [CS] Extra Characters 
--- description: [CS] Extra Characters v7.1\nBy: \\#00ff00\\ULTRA BROS TEAM\n\n\\#ffff00\\Ultra Bros Team:\n\\#ffffff\\FunkyLion\nMelzinoff\nFluffaMario\nSharen\nSquishy\n\n\\#f54e59\\Characters:\n\\#ffffff\\Toadette\nPeach\nDaisy\nYoshi\nBirdo\nForeman Spike\nPauline
+-- name: [CS] Extra Characters
+-- description: [CS] Extra Characters v8.1\nBy: \\#00ff00\\ULTRA BROS TEAM\n\n\\#ffff00\\Ultra Bros Team:\n\\#ffffff\\FunkyLion\nMelzinoff\nFluffaMario\nSharen\nSquishy\n\n\\#f54e59\\Characters:\n\\#ffffff\\Toadette\nPeach\nDaisy\nYoshi\nBirdo\nForeman Spike\nPauline\nRosalina
+
+local TEXT_PACK_NAME = "Extra Characters"
 
 if not _G.charSelectExists then
     djui_popup_create("\\#ffffa0\\Extra Characters requires\nCharacter Select to be enabled.\n\nPlease rehost with it enabled.", 4)
@@ -564,8 +566,8 @@ extraCharacters = {
         palette = {
             [PANTS]  = { r = 0x00, g = 0x00, b = 0xFF }, -- 0000FF
             [SHIRT]  = { r = 0x56, g = 0x8E, b = 0xFF }, -- 568EFF
-            [GLOVES] = { r = 0xFF, g = 0xFF, b = 0x00 }, -- FFFF00
-            [SHOES]  = { r = 0xE0, g = 0xAB, b = 0x2A }, -- E0AB2A
+            [GLOVES] = { r = 0x61, g = 0x3E, b = 0x21 }, -- 613E21
+            [SHOES]  = { r = 0x61, g = 0x3E, b = 0x21 }, -- 613E21
             [HAIR]   = { r = 0x00, g = 0x00, b = 0x00 }, -- 000000
             [SKIN]   = { r = 0xFE, g = 0xC1, b = 0x79 }, -- FEC179
             [CAP]    = { r = 0x56, g = 0x8E, b = 0xFF }, -- 568EFF
@@ -768,18 +770,200 @@ extraCharacters = {
             [CHAR_ANIM_GRAB_BOWSER] = 'princess_grab_bowser'
         }
     },
+    --------------
+    -- Rosalina --
+    --------------
+    {
+        name = "Rosalina",
+        description = "Protector of the cosmos and mother to billions of Lumas. She was once a little girl who lost her own mother after traveling the cosmos to help a little Luma find their own mother. She has taken her role as their mother since.",
+        credits = "Coop Team / VA: GauntletQueen",
+        color = {r = 143, g = 239, b = 246},
+        model = smlua_model_util_get_id("rosalina_geo"),
+        forceChar = CT_MARIO,
+        lifeIcon = get_texture_info("icon-rosalina"),
+        camScale = 1.1,
+        offset = 0,
+        caps = {
+            normal = smlua_model_util_get_id("rosalina_cap_geo"),
+            wing = smlua_model_util_get_id("rosalina_wing_cap_geo"),
+            metal = smlua_model_util_get_id("rosalina_metal_cap_geo"),
+            metalWing = smlua_model_util_get_id("rosalina_metal_wing_cap_geo")
+        },
+        palette = {
+            [PANTS]  = { r = 0x8F, g = 0xEF, b = 0xF6 }, -- 8FEFF6
+            [SHIRT]  = { r = 0x8F, g = 0xEF, b = 0xF6 }, -- 8FEFF6
+            [GLOVES] = { r = 0xFF, g = 0xFF, b = 0xFF }, -- FFFFFF
+            [SHOES]  = { r = 0xFF, g = 0xFF, b = 0x00 }, -- FFFF00
+            [HAIR]   = { r = 0xFC, g = 0xC6, b = 0x9A }, -- FCC69A
+            [SKIN]   = { r = 0xFC, g = 0xE3, b = 0xCD }, -- FCE3CD
+            [CAP]    = { r = 0xB7, g = 0xB7, b = 0xBB }, -- B7B7BB
+            [EMBLEM] = { r = 0xFF, g = 0xFF, b = 0x00 }  -- FFFF00
+        },
+        voices = {
+            [CHAR_SOUND_ATTACKED] = "rosalina_attacked.ogg",
+            [CHAR_SOUND_COUGHING1] = "rosalina_coughing1.ogg",
+            [CHAR_SOUND_COUGHING2] = "rosalina_coughing2.ogg",
+            [CHAR_SOUND_COUGHING3] = "rosalina_coughing3.ogg",
+            [CHAR_SOUND_DOH] = "rosalina_doh.ogg",
+            [CHAR_SOUND_DROWNING] = "rosalina_drowning.ogg",
+            [CHAR_SOUND_DYING] = "rosalina_dying.ogg",
+            [CHAR_SOUND_EEUH] = "rosalina_eeuh.ogg",
+            [CHAR_SOUND_GROUND_POUND_WAH] = "rosalina_ground_pound_wah.ogg",
+            [CHAR_SOUND_HAHA] = "rosalina_haha.ogg",
+            [CHAR_SOUND_HAHA_2] = "rosalina_haha.ogg",
+            [CHAR_SOUND_HERE_WE_GO] = "rosalina_herewego.ogg",
+            [CHAR_SOUND_HOOHOO] = "rosalina_hoohoo.ogg",
+            [CHAR_SOUND_HRMM] = "rosalina_hrmm.ogg",
+            [CHAR_SOUND_IMA_TIRED] = "rosalina_imatired.ogg",
+            [CHAR_SOUND_MAMA_MIA] = "rosalina_mamamia.ogg",
+            [CHAR_SOUND_LETS_A_GO] = "rosalina_letsago.ogg",
+            [CHAR_SOUND_ON_FIRE] = "rosalina_on_fire.ogg",
+            [CHAR_SOUND_OOOF] = "rosalina_ooof.ogg",
+            [CHAR_SOUND_OOOF2] = "rosalina_ooof2.ogg",
+            [CHAR_SOUND_PANTING] = "rosalina_panting.ogg",
+            [CHAR_SOUND_PANTING_COLD] = "rosalina_panting_cold.ogg",
+            [CHAR_SOUND_PUNCH_HOO] = "rosalina_punch_hoo.ogg",
+            [CHAR_SOUND_PUNCH_WAH] = "rosalina_punch_wah.ogg",
+            [CHAR_SOUND_PUNCH_YAH] = "rosalina_punch_yah.ogg",
+            [CHAR_SOUND_SO_LONGA_BOWSER] = "rosalina_solonga_bowser.ogg",
+            [CHAR_SOUND_SNORING1] = "rosalina_snoring1.ogg",
+            [CHAR_SOUND_SNORING2] = "rosalina_snoring2.ogg",
+            [CHAR_SOUND_SNORING3] = {"rosalina_snoring2.ogg", "rosalina_snoring1.ogg", "rosalina_snoring3.ogg"},
+            [CHAR_SOUND_TWIRL_BOUNCE] = "rosalina_twirl_bounce.ogg",
+            [CHAR_SOUND_UH] = "rosalina_uh.ogg",
+            [CHAR_SOUND_UH2] = "rosalina_uh2.ogg",
+            [CHAR_SOUND_UH2_2] = "rosalina_uh2_2.ogg",
+            [CHAR_SOUND_WAAAOOOW] = "rosalina_waaaooow.ogg",
+            [CHAR_SOUND_WAH2] = "rosalina_ground_pound_wah.ogg",
+            [CHAR_SOUND_WHOA] = "rosalina_whoa.ogg",
+            [CHAR_SOUND_YAHOO] = "rosalina_yahoo.ogg",
+            [CHAR_SOUND_YAWNING] = "rosalina_yawning.ogg",
+            [CHAR_SOUND_YAHOO_WAHA_YIPPEE] = { "rosalina_yahoo.ogg", "rosalina_yahoo1.ogg", "rosalina_yahoo2.ogg", "rosalina_yahoo3.ogg", "rosalina_yahoo4.ogg", "rosalina_yahoo5.ogg" },
+            [CHAR_SOUND_YAH_WAH_HOO] = { "rosalina_yah_wah_hoo1.ogg", "rosalina_yah_wah_hoo2.ogg", "rosalina_yah_wah_hoo3.ogg"},
+            [CHAR_SOUND_HELLO] = {"rosalina_hello.ogg", "rosalina_hello1.ogg", "rosalina_hello2.ogg", "rosalina_hello3.ogg", "rosalina_hello4.ogg"}
+        },
+        anims = {
+            [CHAR_ANIM_RUNNING] = 'rosalina_running',
+            [CHAR_ANIM_IDLE_HEAD_CENTER] = 'princess_idle_head_center',
+            [CHAR_ANIM_IDLE_HEAD_LEFT] = 'princess_idle_head_left',
+            [CHAR_ANIM_IDLE_HEAD_RIGHT] = 'princess_idle_head_right',
+            [CHAR_ANIM_BACKWARD_KB] = 'princess_backward_kb',
+            [CHAR_ANIM_FIRST_PERSON] = 'princess_first_person',
+            [CHAR_ANIM_FLUTTERKICK] = 'princess_flutterkick',
+            [CHAR_ANIM_FLUTTERKICK_WITH_OBJ] = 'princess_flutterkick_with_obj',
+            [CHAR_ANIM_FORWARD_KB] = 'princess_forward_kb',
+            [CHAR_ANIM_GROUND_BONK] = 'princess_ground_bonk',
+            [CHAR_ANIM_GROUND_KICK] = 'princess_ground_kick',
+            [CHAR_ANIM_GROUND_THROW] = 'princess_ground_throw',
+            [CHAR_ANIM_HEAVY_THROW] = 'princess_heavy_throw',
+            [CHAR_ANIM_MISSING_CAP] = 'princess_missing_cap',
+            [CHAR_ANIM_PULL_DOOR_WALK_IN] = 'princess_pull_door_walk_in',
+            [CHAR_ANIM_FIRST_PERSON] = 'princess_first_person',
+            [CHAR_ANIM_RELEASE_BOWSER] = 'princess_release_bowser',
+            [CHAR_ANIM_RETURN_FROM_STAR_DANCE] = 'princess_return_from_star_dance',
+            [CHAR_ANIM_RETURN_STAR_APPROACH_DOOR] = 'princess_return_star_approach_door',
+            [CHAR_ANIM_SLIDEFLIP_LAND] = 'princess_sideflip_land',
+            [CHAR_ANIM_SLEEP_IDLE] = 'princess_sleep_idle',
+            [CHAR_ANIM_START_SLEEP_SITTING] = 'princess_sleep_sitting',
+            [CHAR_ANIM_SOFT_BACK_KB] = 'princess_soft_back_kb',
+            [CHAR_ANIM_SOFT_FRONT_KB] = 'princess_soft_front_kb',
+            [CHAR_ANIM_STAR_DANCE] = 'princess_star_dance',
+            [CHAR_ANIM_START_CROUCHING] = 'princess_start_crouch',
+            [CHAR_ANIM_STOP_CROUCHING] = 'princess_stop_crouch',
+            [CHAR_ANIM_START_SLEEP_IDLE] = 'princess_start_sleep',
+            [CHAR_ANIM_STOP_SLIDE] = 'princess_stop_slide',
+            [CHAR_ANIM_SUMMON_STAR] = 'princess_summon_star',
+            [CHAR_ANIM_SWIM_WITH_OBJ_PART1] = 'princess_swim_with_obj1',
+            [CHAR_ANIM_SWIM_WITH_OBJ_PART2] = 'princess_swim_with_obj2',
+            [CHAR_ANIM_TRIPLE_JUMP_LAND] = 'princess_triple_jump_land',
+            [CHAR_ANIM_TURNING_PART1] = 'princess_turning_part1',
+            [CHAR_ANIM_TURNING_PART2] = 'princess_turning_part2',
+            [CHAR_ANIM_TWIRL_LAND] = 'princess_twirl_land',
+            [CHAR_ANIM_WAKE_FROM_LYING] = 'princess_wake_from_lying',
+            [CHAR_ANIM_WAKE_FROM_SLEEP] = 'princess_wake_from_sleep',
+            [CHAR_ANIM_CREDITS_RAISE_HAND] = 'princess_credits_raise_hand',
+            [CHAR_ANIM_CREDITS_RETURN_FROM_LOOK_UP] = 'princess_credits_return_from_look_up',
+            [CHAR_ANIM_CREDITS_START_WALK_LOOK_UP] = 'princess_credits_start_walk_look_up',
+            [CHAR_ANIM_CREDITS_TAKE_OFF_CAP] = 'princess_credits_take_off_cap',
+            [CHAR_ANIM_DYING_FALL_OVER] = 'princess_dying_fall_over',
+            [CHAR_ANIM_FALL_OVER_BACKWARDS] = 'princess_fall_over_backwards',
+            [CHAR_ANIM_FAST_LEDGE_GRAB] = 'princess_fast_ledge_grab',
+            --[CHAR_ANIM_FINAL_BOWSER_RAISE_HAND_SPIN] = 'princess_final_bowser_raise_hand_spin',
+            [CHAR_ANIM_FIRST_PUNCH_FAST] = 'princess_first_punch_fast',
+            [CHAR_ANIM_GENERAL_LAND] = 'princess_general_land',
+            [CHAR_ANIM_HEAD_STUCK_IN_GROUND] = 'princess_head_stuck_in_ground',
+            [CHAR_ANIM_LAND_FROM_DOUBLE_JUMP] = 'princess_land_from_double_jump',
+            [CHAR_ANIM_LAND_FROM_SINGLE_JUMP] = 'princess_land_from_single_jump',
+            [CHAR_ANIM_LAND_ON_STOMACH] = 'princess_land_on_stomach',
+            [CHAR_ANIM_LEGS_STUCK_IN_GROUND] = 'princess_legs_stuck_in_ground',
+            [CHAR_ANIM_PLACE_LIGHT_OBJ] = 'princess_place_light_obj',
+            [CHAR_ANIM_PUSH_DOOR_WALK_IN] = 'princess_push_door_walk_in',
+            [CHAR_ANIM_PUT_CAP_ON] = 'princess_put_cap_on',
+            [CHAR_ANIM_RUN_WITH_LIGHT_OBJ] = 'princess_run_with_light_obj',
+            [CHAR_ANIM_SECOND_PUNCH_FAST] = 'princess_second_punch_fast',
+            [CHAR_ANIM_SHIVERING_RETURN_TO_IDLE] = 'princess_shivering_returning_to_idle',
+            [CHAR_ANIM_SKID_ON_GROUND] = 'princess_skid_on_ground',
+            [CHAR_ANIM_SLOW_LAND_FROM_DIVE] = 'princess_slow_land_from_dive',
+            [CHAR_ANIM_SLOW_LEDGE_GRAB] = 'princess_slow_ledge_grab',
+            [CHAR_ANIM_SLOW_WALK_WITH_LIGHT_OBJ] = 'princess_slow_walk_with_light_obj',
+            [CHAR_ANIM_STAND_UP_FROM_LAVA_BOOST] = 'princess_stand_up_from_lava_boost',
+            [CHAR_ANIM_STOP_SKID] = 'princess_stop_skid',
+            [CHAR_ANIM_TAKE_CAP_OFF_THEN_ON] = 'princess_take_cap_off_then_on',
+            [CHAR_ANIM_THROW_CATCH_KEY] = 'princess_throw_catch_key',
+            [CHAR_ANIM_WALK_WITH_LIGHT_OBJ] = 'princess_walk_with_light_obj',
+            [CHAR_ANIM_BOTTOM_STUCK_IN_GROUND] = 'princess_bottom_stuck_in_ground',
+            --[CHAR_ANIM_CREDITS_PEACE_SIGN] = 'princess_credits_peace_sign',
+            [CHAR_ANIM_FIRE_LAVA_BURN] = 'princess_fire_lava_burn',
+            [CHAR_ANIM_GROUND_POUND] = 'princess_ground_pound',
+            [CHAR_ANIM_GROUND_POUND_LANDING] = 'princess_ground_pound_landing',
+            [CHAR_ANIM_TRIPLE_JUMP_GROUND_POUND] = 'princess_triple_jump_ground_pound_anim',
+            [CHAR_ANIM_UNLOCK_DOOR] = 'princess_unlock_door',
+            [CHAR_ANIM_SWINGING_BOWSER] = 'princess_swinging_bowser',
+            [CHAR_ANIM_HOLDING_BOWSER] = 'princess_holding_bowser',
+            [CHAR_ANIM_GRAB_BOWSER] = 'princess_grab_bowser',
+            [CHAR_ANIM_BEND_KNESS_RIDING_SHELL] = 'rosalina_spinjump',
+        }
+    },
 }
+
+local TEXT_VOICE_ACTOR = " (Voice Actors)"
+local TEXT_MOVESET     = " (Movesets)"
 
 local CSloaded = false
 local function on_character_select_load()
     for i = 1, #extraCharacters do
         local char = extraCharacters[i]
-        char.tablePos = _G.charSelect.character_add(char.name, char.description, char.credits, char.color, char.model, char.forceChar, char.lifeIcon, char.camScale, char.offset)
+        char.tablePos = _G.charSelect.character_add(char.name, char.description, char.credits, char.color, char.model, char.forceChar, char.lifeIcon, char.camScale, char.offset, char.meter)
         if char.caps then _G.charSelect.character_add_caps(char.model, char.caps) end
         if char.voices then _G.charSelect.character_add_voice(char.model, char.voices) end
         if char.palette then _G.charSelect.character_add_palette_preset(char.model, char.palette) end
         if char.anims then _G.charSelect.character_add_animations(char.model, char.anims) end
+        -- if char.meter then _G.charSelect.character_add_health_meter(char.model, char.meter) end
     end
+
+    _G.charSelect.credit_add(TEXT_PACK_NAME, "FunkyLion", "Lead Dev")
+    _G.charSelect.credit_add(TEXT_PACK_NAME, "Melzinoff", "Models / Animation")
+    _G.charSelect.credit_add(TEXT_PACK_NAME, "Sharen", "Animation")
+    _G.charSelect.credit_add(TEXT_PACK_NAME, "FluffaMario", "Models")
+    _G.charSelect.credit_add(TEXT_PACK_NAME, 'Strawberii "Oreo"', "Icons")
+    _G.charSelect.credit_add(TEXT_PACK_NAME, "Chars_64", "Icons")
+    _G.charSelect.credit_add(TEXT_PACK_NAME, "WaterVapor", "Icons")
+    _G.charSelect.credit_add(TEXT_PACK_NAME, "Squishy6094", "Code Rewrite")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_VOICE_ACTOR, "MelissaMekrose", "Toadette")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_VOICE_ACTOR, "SuperKirbyLover", "Peach")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_VOICE_ACTOR, "MorphiGalaxi", "Daisy")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_VOICE_ACTOR, "FunkyLion", "Yoshi")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_VOICE_ACTOR, "LuUvvUCY", "Birdo")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_VOICE_ACTOR, "VinnyVinesauce", "Spike")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_VOICE_ACTOR, "BeckyVO", "Pauline")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_VOICE_ACTOR, "GauntletQueen", "Rosalina")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_MOVESET, "xLuigiGamerx", "Toadette")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_MOVESET, "Wibblus", "Peach")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_MOVESET, "Wibblus", "Daisy")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_MOVESET, "xLuigiGamerx", "Yoshi")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_MOVESET, "EmilyEmmi", "Birdo")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_MOVESET, "Squishy6094", "Pauline")
+    _G.charSelect.credit_add(TEXT_PACK_NAME..TEXT_MOVESET, "Wibblus", "Rosalina")
 
     CSloaded = true
 end
@@ -791,13 +975,21 @@ local function on_character_sound(m, sound)
     end
 end
 
-local function on_character_snore(m)
+--Used for the final Extra Character
+--[[local function mario_update(m)
     if not CSloaded then return end
+    if m.action == ACT_WALKING and _G.charSelect.character_get_current_number(m.playerIndex) == extraCharacters[10].tablePos then
+        m.marioBodyState.torsoAngle.x = 0
+        m.marioBodyState.torsoAngle.z = 0
+        --results in double step sounds when going at the usual 32 speed (sigh)
+        m.marioObj.header.gfx.animInfo.animAccel = m.marioObj.header.gfx.animInfo.animAccel * 0.85
+    end
     for i = 1, #extraCharacters do
         if _G.charSelect.character_get_voice(m) == extraCharacters[i].voices then return _G.charSelect.voice.snore(m) end
     end
 end
+]]
 
 hook_event(HOOK_ON_MODS_LOADED, on_character_select_load)
 hook_event(HOOK_CHARACTER_SOUND, on_character_sound)
-hook_event(HOOK_MARIO_UPDATE, on_character_snore)
+--hook_event(HOOK_MARIO_UPDATE, mario_update)
