5891f65106337ac9be6488ec656fc5b73a98827c
Baconator2558 <77943363+Baconator2558@users.noreply.github.com>
2025-03-27 09:46:34 -0500

    Update mario_actions_stationary.c

--- src/game/mario_actions_stationary.c
+++ src/game/mario_actions_stationary.c
@@ -1151,7 +1151,7 @@ s32 act_first_person(struct MarioState *m) {
 s32 check_common_stationary_cancels(struct MarioState *m) {
     if (!m) { return 0; }
     if (m->playerIndex != 0) { return FALSE; }
-    bool allow = TRUE;
+    bool allow = true;
     smlua_call_event_hooks_mario_param_and_int_ret_bool(HOOK_ALLOW_FORCE_WATER_ACTION, m, ACT_GROUP_STATIONARY, &allow);
     if (allow && m->pos[1] < m->waterLevel - 100) {
         if (m->action == ACT_SPAWN_SPIN_LANDING) {
