c36b034b690a402a1693b1483c1efac0288cbbc8
Baconator2558 <77943363+Baconator2558@users.noreply.github.com>
2025-03-27 09:11:52 -0500

    Update mario_actions_airborne.c

--- src/game/mario_actions_airborne.c
+++ src/game/mario_actions_airborne.c
@@ -2299,7 +2299,7 @@ s32 check_common_airborne_cancels(struct MarioState *m) {
     if (!m) { return 0; }
     bool allow = TRUE;
     smlua_call_event_hooks_mario_param_and_int_ret_bool(HOOK_ALLOW_FORCE_WATER_ACTION, m, ACT_GROUP_AIRBORNE, &allow);
-    if (water && m->pos[1] < m->waterLevel - 100) {
+    if (allow && m->pos[1] < m->waterLevel - 100) {
         return set_water_plunge_action(m);
     }
     allow = TRUE;
