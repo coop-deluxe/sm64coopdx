85c8b4ac8d140c03a0cf47e50f47a0202dfbbba0
Baconator2558 <77943363+Baconator2558@users.noreply.github.com>
2025-03-27 09:37:43 -0500

    Update mario_actions_moving.c

--- src/game/mario_actions_moving.c
+++ src/game/mario_actions_moving.c
@@ -2174,7 +2174,9 @@ Performs common checks when Mario is in a moving state, transitions to water plu
 |descriptionEnd| */
 s32 check_common_moving_cancels(struct MarioState *m) {
     if (!m) { return FALSE; }
-    if (m->pos[1] < m->waterLevel - 100) {
+    bool allow = TRUE;
+    smlua_call_event_hooks_mario_param_and_int_ret_bool(HOOK_ALLOW_FORCE_WATER_ACTION, m, ACT_GROUP_MOVING, &allow);
+    if (allow && m->pos[1] < m->waterLevel - 100) {
         return set_water_plunge_action(m);
     }
 
