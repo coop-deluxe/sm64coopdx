5de8c9a0c3ed30e046cee64c4a3a682321080ce6
Baconator2558 <77943363+Baconator2558@users.noreply.github.com>
2025-04-05 13:32:21 -0500

    Update mario_actions_object.c

--- src/game/mario_actions_object.c
+++ src/game/mario_actions_object.c
@@ -479,7 +479,7 @@ s32 check_common_object_cancels(struct MarioState *m) {
     if (m->playerIndex != 0) { return FALSE; }
 
     bool allow = true;
-    smlua_call_event_hooks_mario_param_and_int_ret_bool(HOOK_ALLOW_FORCE_WATER_ACTION, m, false, &allow);
+    smlua_call_event_hooks_mario_param_and_bool_ret_bool(HOOK_ALLOW_FORCE_WATER_ACTION, m, false, &allow);
     if (allow && m->pos[1] <  m->waterLevel - 100) {
         return set_water_plunge_action(m);
     }
