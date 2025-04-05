589dbfbc9455430ca39a69fef965a52bf4a85117
Baconator2558 <77943363+Baconator2558@users.noreply.github.com>
2025-04-05 12:16:03 -0500

    Add files via upload

--- src/game/mario_actions_airborne.c
+++ src/game/mario_actions_airborne.c
@@ -2298,7 +2298,7 @@ if on certain wind surfaces. Also resets `m.quicksandDepth`
 s32 check_common_airborne_cancels(struct MarioState *m) {
     if (!m) { return 0; }
     bool allow = true;
-    smlua_call_event_hooks_mario_param_and_int_ret_bool(HOOK_ALLOW_FORCE_WATER_ACTION, m, ACT_GROUP_AIRBORNE, &allow);
+    smlua_call_event_hooks_mario_param_and_int_ret_bool(HOOK_ALLOW_FORCE_WATER_ACTION, m, 0, &allow);
     if (allow && m->pos[1] < m->waterLevel - 100) {
         return set_water_plunge_action(m);
     }
--- src/game/mario_actions_submerged.c
+++ src/game/mario_actions_submerged.c
@@ -205,7 +205,7 @@ u32 perform_water_step(struct MarioState *m) {
     nextPos[2] = m->pos[2] + step[2];
 
     bool allow = true;
-    smlua_call_event_hooks_mario_param_ret_bool(HOOK_ALLOW_FORCE_NON_WATER_ACTION, m, &allow); 
+    smlua_call_event_hooks_mario_param_and_int_ret_bool(HOOK_ALLOW_FORCE_WATER_ACTION, m, 1, &allow); 
     if (allow && nextPos[1] > m->waterLevel - 80) {
         nextPos[1] = m->waterLevel - 80;
         m->vel[1] = 0.0f;
@@ -528,7 +528,7 @@ static void play_swimming_noise(struct MarioState *m) {
 static s32 check_water_jump(struct MarioState *m) {
     if (!m) { return 0; }
     bool allow = true;
-    smlua_call_event_hooks_mario_param_ret_bool(HOOK_ALLOW_FORCE_NON_WATER_ACTION, m, &allow); 
+    smlua_call_event_hooks_mario_param_and_int_ret_bool(HOOK_ALLOW_FORCE_WATER_ACTION, m, 1, &allow); 
     if (!allow) { return FALSE; }
     s32 probe = (s32)(m->pos[1] + 1.5f);
 
@@ -1619,7 +1619,7 @@ static s32 act_hold_metal_water_fall_land(struct MarioState *m) {
 static s32 check_common_submerged_cancels(struct MarioState *m) {
     if (!m) { return 0; }
     bool allow = true;
-    smlua_call_event_hooks_mario_param_ret_bool(HOOK_ALLOW_FORCE_NON_WATER_ACTION, m, &allow);
+    smlua_call_event_hooks_mario_param_and_int_ret_bool(HOOK_ALLOW_FORCE_WATER_ACTION, m, 1, &allow);
     if (allow && m->pos[1] > m->waterLevel - 80) {
         if (m->waterLevel - 80 > m->floorHeight) {
             m->pos[1] = m->waterLevel - 80;
