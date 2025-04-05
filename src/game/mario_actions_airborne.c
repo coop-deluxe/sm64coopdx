85618dca847c59bd6f30b2a3a10d359465fe407e
Baconator2558 <77943363+Baconator2558@users.noreply.github.com>
2025-04-05 16:52:00 -0500

    Update mario_actions_airborne.c

--- src/game/mario_actions_airborne.c
+++ src/game/mario_actions_airborne.c
@@ -2298,9 +2298,9 @@ if on certain wind surfaces. Also resets `m.quicksandDepth`
 s32 check_common_airborne_cancels(struct MarioState *m) {
     if (!m) { return 0; }
     bool allow = true;
-    smlua_call_event_hooks_mario_param_and_bool_ret_bool(HOOK_ALLOW_FORCE_WATER_ACTION, m, false, &allow);
-    if (allow && m->pos[1] < m->waterLevel - 100) {
-        return set_water_plunge_action(m);
+    if (m->pos[1] < m->waterLevel - 100) {
+        smlua_call_event_hooks_mario_param_and_bool_ret_bool(HOOK_ALLOW_FORCE_WATER_ACTION, m, false, &allow);
+        return allow && set_water_plunge_action(m);
     }
     allow = true;
 
@@ -2308,9 +2308,10 @@ s32 check_common_airborne_cancels(struct MarioState *m) {
         return drop_and_set_mario_action(m, ACT_SQUISHED, 0);
     }
 
-    smlua_call_event_hooks_mario_param_and_int_ret_bool(HOOK_ALLOW_HAZARD_SURFACE, m, HAZARD_TYPE_VERTICAL_WIND, &allow);
-    if (allow && m->floor && m->floor->type == SURFACE_VERTICAL_WIND && (m->action & ACT_FLAG_ALLOW_VERTICAL_WIND_ACTION)) {
-        return drop_and_set_mario_action(m, ACT_VERTICAL_WIND, 0);
+    
+    if (m->floor && m->floor->type == SURFACE_VERTICAL_WIND && (m->action & ACT_FLAG_ALLOW_VERTICAL_WIND_ACTION)) {
+        smlua_call_event_hooks_mario_param_and_int_ret_bool(HOOK_ALLOW_HAZARD_SURFACE, m, HAZARD_TYPE_VERTICAL_WIND, &allow);
+        return allow && drop_and_set_mario_action(m, ACT_VERTICAL_WIND, 0);
     }
 
     m->quicksandDepth = 0.0f;
