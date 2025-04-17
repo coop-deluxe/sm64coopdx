6c7068f1ea80f7774a41a4fc121f51c893c799e8
Agent X <44549182+AgentXLP@users.noreply.github.com>
2025-04-17 18:27:36 -0400

    Revert "Constants fix"
    This reverts commit 6659377cd40e9a86b1fcb81db15cb29cc45b01f7.

--- autogen/convert_constants.py
+++ autogen/convert_constants.py
@@ -66,7 +66,6 @@ include_constants = {
     "src/audio/external.h": [ "SEQ_PLAYER", "DS_" ],
     "src/pc/mods/mod_storage.h": [ "MAX_KEYS", "MAX_KEY_VALUE_LENGTH" ],
     "include/PR/gbi.h": [
-        "G_IMMFIRST",
         "^G_NOOP$",
         "^G_SETOTHERMODE_H$",
         "^G_SETOTHERMODE_L$",
