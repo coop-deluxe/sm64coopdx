162625fb0375acc9fcc13548efcb79f059982fdd
Sunk <69110309+Sunketchupm@users.noreply.github.com>
2025-05-18 20:51:50 -0400

    Re-add fix collision options in debug

--- src/pc/djui/djui_panel_misc.c
+++ src/pc/djui/djui_panel_misc.c
@@ -12,13 +12,19 @@
 #include <windows.h>
 #endif
 
+#ifdef DEVELOPMENT
+void fix_collision_bugs_wrapper(UNUSED struct DjuiBase* caller) {
+    fix_collision_bugs_set_all(gLevelValues.fixCollision.throwaway);
+}
+#endif
+
 void djui_panel_options_debug_create(struct DjuiBase* caller) {
     struct DjuiThreePanel* panel = djui_panel_menu_create(DLANG(MISC, DEBUG_TITLE), false);
     struct DjuiBase* body = djui_three_panel_get_body(panel);
     {
         djui_checkbox_create(body, DLANG(MISC, LUA_PROFILER), &configLuaProfiler, NULL);
 #ifdef DEVELOPMENT
-        //djui_checkbox_create(body, DLANG(MISC, FIXED_COLLISIONS), (bool*)&gLevelValues.fixCollisionBugs, NULL);
+        djui_checkbox_create(body, DLANG(MISC, FIXED_COLLISIONS), (bool*)&gLevelValues.fixCollision.throwaway, fix_collision_bugs_wrapper);
         djui_checkbox_create(body, DLANG(MISC, CTX_PROFILER), &configCtxProfiler, NULL);
         djui_checkbox_create(body, DLANG(MISC, DEBUG_PRINT), &configDebugPrint, NULL);
         djui_checkbox_create(body, DLANG(MISC, DEBUG_INFO), &configDebugInfo, NULL);
