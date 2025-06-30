59f674c2b9fdf397914c7ffe8a3e296a63877450
Isaac0-dev <62234577+Isaac0-dev@users.noreply.github.com>
2025-06-30 13:18:12 +1000

    using peachy's changes

--- include/macros.h
+++ include/macros.h
@@ -128,12 +128,4 @@
 #define LIST_ARGS_9(ACTION) LIST_ARGS_8(ACTION), ACTION(9)
 #define LIST_ARGS(ACTION, N) LIST_ARGS_##N(ACTION)
 
-// +1 indexed
-#define PP_ARG_N(  _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, \
-                   _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, N, ...) N
-#define PP_COUNT_ARGS(...) PP_ARG_N(,##__VA_ARGS__, \
-                                   20,19,18,17,16,15,14,13,12,11, \
-                                   10,9,8,7,6,5,4,3,2,1,0)
-#define COUNT_ARGS(...) PP_COUNT_ARGS(__VA_ARGS__)
-
 #endif // MACROS_H
