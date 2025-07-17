b76985eb645deeef705cb48d33f59c1fb3c13fe7
Agent X <44549182+AgentXLP@users.noreply.github.com>
2025-07-17 15:41:55 -0400

    Fix

--- autogen/lua_definitions/constants.lua
+++ autogen/lua_definitions/constants.lua
@@ -11119,7 +11119,7 @@ COOP_OBJ_FLAG_NON_SYNC = (1 << 2)
 COOP_OBJ_FLAG_INITIALIZED = (1 << 3)
 
 --- @type string
-SM64COOPDX_VERSION = "v1.4.0"
+SM64COOPDX_VERSION = "v1.4"
 
 --- @type string
 VERSION_TEXT = "v"
