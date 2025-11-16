9f23c45d552f100313155d52177dcf8533e2ed81
MysterD <myster@d>
2025-11-16 14:07:56 -0800

    Move cap music out of if statement

---
+++ developer/network.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+if [ $# -eq 0 ]; then
+    make DEBUG=1 DEVELOPMENT=1 STRICT=1 -j
+else
+    make DEBUG=1 DEVELOPMENT=1 -j
+fi
+
+# find file
+FILE=./build/us_pc/sm64coopdx.exe
+if [ ! -f "$FILE" ]; then
+    FILE=./build/us_pc/sm64coopdx
+fi
+
+$FILE --console --server 7777 &
+
+$FILE --console --configfile client --client 127.0.0.1 7777 &
