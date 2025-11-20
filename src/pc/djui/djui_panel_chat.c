b00e12aa3bdfd5eddd1581f4f6c6f27dc7fbe5a7
iZePlayz <69536095+iZePlayzYT@users.noreply.github.com>
2025-11-20 22:32:56 +0100

    added more chat size (height/width) options

--- src/pc/djui/djui_panel_chat.c
+++ src/pc/djui/djui_panel_chat.c
@@ -13,42 +13,54 @@ void djui_panel_chat_create(struct DjuiBase* caller) {
         djui_checkbox_create(body, DLANG(CHAT_OPTIONS, CHAT_CHAR_COUNTER), &configChatCharCounter, NULL);
         djui_checkbox_create(body, DLANG(CHAT_OPTIONS, DISABLE_CHAT_WHEN_CLOSED), &configDisableChatWhenClosed, NULL);
 
-        static unsigned int sChatWidthIndex = 3;
+        static unsigned int sChatWidthIndex = 5;
         switch (configChatWidth) {
-            case 500: sChatWidthIndex = 0; break;
-            case 600: sChatWidthIndex = 1; break;
-            case 700: sChatWidthIndex = 2; break;
-            case 800: sChatWidthIndex = 3; break;
-            case 900: sChatWidthIndex = 4; break;
-            case 1000: sChatWidthIndex = 5; break;
-            case 1100: sChatWidthIndex = 6; break;
-            default: sChatWidthIndex = 3; break;
+            case 300: sChatWidthIndex = 0; break;
+            case 400: sChatWidthIndex = 1; break;
+            case 500: sChatWidthIndex = 2; break;
+            case 600: sChatWidthIndex = 3; break;
+            case 700: sChatWidthIndex = 4; break;
+            case 800: sChatWidthIndex = 5; break;
+            case 900: sChatWidthIndex = 6; break;
+            case 1000: sChatWidthIndex = 7; break;
+            case 1100: sChatWidthIndex = 8; break;
+            case 1200: sChatWidthIndex = 9; break;
+            case 1300: sChatWidthIndex = 10; break;
+            default: sChatWidthIndex = 5; break;
         }
 
-        static unsigned int sChatHeightIndex = 3;
+        static unsigned int sChatHeightIndex = 5;
         switch (configChatHeight) {
-            case 250: sChatHeightIndex = 0; break;
-            case 300: sChatHeightIndex = 1; break;
-            case 350: sChatHeightIndex = 2; break;
-            case 400: sChatHeightIndex = 3; break;
-            case 450: sChatHeightIndex = 4; break;
-            case 500: sChatHeightIndex = 5; break;
-            case 550: sChatHeightIndex = 6; break;
-            default: sChatHeightIndex = 3; break;
+            case 200: sChatHeightIndex = 0; break;
+            case 250: sChatHeightIndex = 1; break;
+            case 300: sChatHeightIndex = 2; break;
+            case 350: sChatHeightIndex = 3; break;
+            case 400: sChatHeightIndex = 4; break;
+            case 450: sChatHeightIndex = 5; break;
+            case 500: sChatHeightIndex = 6; break;
+            case 550: sChatHeightIndex = 7; break;
+            case 600: sChatHeightIndex = 8; break;
+            case 650: sChatHeightIndex = 9; break;
+            case 700: sChatHeightIndex = 10; break;
+            default: sChatHeightIndex = 4; break;
         }
 
         char* chatSizeChoices[] = {
+            DLANG(CHAT_OPTIONS, CHAT_SIZE_MINIMUM),
+            DLANG(CHAT_OPTIONS, CHAT_SIZE_VERY_TINY),
             DLANG(CHAT_OPTIONS, CHAT_SIZE_TINY),
             DLANG(CHAT_OPTIONS, CHAT_SIZE_VERY_SMALL),
             DLANG(CHAT_OPTIONS, CHAT_SIZE_SMALL),
             DLANG(CHAT_OPTIONS, CHAT_SIZE_NORMAL),
             DLANG(CHAT_OPTIONS, CHAT_SIZE_BIG),
             DLANG(CHAT_OPTIONS, CHAT_SIZE_VERY_BIG),
-            DLANG(CHAT_OPTIONS, CHAT_SIZE_HUGE)
+            DLANG(CHAT_OPTIONS, CHAT_SIZE_HUGE),
+            DLANG(CHAT_OPTIONS, CHAT_SIZE_VERY_HUGE),
+            DLANG(CHAT_OPTIONS, CHAT_SIZE_MAXIMUM)
         };
         void on_chat_width_change(UNUSED struct DjuiBase* b) {
             unsigned int idx = sChatWidthIndex;
-            unsigned int widths[] = { 500, 600, 700, 800, 900, 1000, 1100 };
+            unsigned int widths[] = { 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300 };
             configChatWidth = widths[idx];
             if (gDjuiChatBox != NULL) {
                 djui_base_set_size(&gDjuiChatBox->base, configChatWidth, gDjuiChatBox->base.height.value);
@@ -56,14 +68,14 @@ void djui_panel_chat_create(struct DjuiBase* caller) {
         }
         void on_chat_height_change(UNUSED struct DjuiBase* b) {
             unsigned int idx = sChatHeightIndex;
-            unsigned int heights[] = { 250, 300, 350, 400, 450, 500, 550 };
+            unsigned int heights[] = { 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700 };
             configChatHeight = heights[idx];
             if (gDjuiChatBox != NULL) {
                 djui_base_set_size(&gDjuiChatBox->base, gDjuiChatBox->base.width.value, configChatHeight);
             }
         }
-        djui_selectionbox_create(body, DLANG(CHAT_OPTIONS, CHAT_WIDTH),  chatSizeChoices, 7, &sChatWidthIndex,  on_chat_width_change);
-        djui_selectionbox_create(body, DLANG(CHAT_OPTIONS, CHAT_HEIGHT), chatSizeChoices, 7, &sChatHeightIndex, on_chat_height_change);
+        djui_selectionbox_create(body, DLANG(CHAT_OPTIONS, CHAT_WIDTH),  chatSizeChoices, 11, &sChatWidthIndex,  on_chat_width_change);
+        djui_selectionbox_create(body, DLANG(CHAT_OPTIONS, CHAT_HEIGHT), chatSizeChoices, 11, &sChatHeightIndex, on_chat_height_change);
 
         djui_button_create(body, DLANG(MENU, BACK), DJUI_BUTTON_STYLE_BACK, djui_panel_menu_back);
     }
