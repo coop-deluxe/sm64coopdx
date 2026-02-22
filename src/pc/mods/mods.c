d2b781b37b7ff4b2d8f2a4dc6b14f116946a7841
EmeraldLockdown <86802223+EmeraldLoc@users.noreply.github.com>
2026-02-22 14:08:37 -0600

    Add languages, prioritize romhacks aswell when selecting mode text

--- src/pc/mods/mods.c
+++ src/pc/mods/mods.c
@@ -29,6 +29,7 @@ struct LocalEnabledPath {
 struct LocalEnabledPath* sLocalEnabledPaths = NULL;
 
 void mods_get_main_mod_name(char* destination, u32 maxSize) {
+    struct Mod* selectedRomhack = NULL;
     struct Mod* picked = NULL;
     size_t pickedSize = 0;
 
@@ -41,6 +42,11 @@ void mods_get_main_mod_name(char* destination, u32 maxSize) {
             picked = mod;
             break;
         }
+        // prioritize romhacks
+        if ((mod->category && strcmp(mod->category, "romhack") == 0)
+        || (mod->incompatible && strcmp(mod->incompatible, "romhack") == 0)) {
+            selectedRomhack = mod;
+        }
         size_t size = mod_get_lua_size(mod);
         if (size > pickedSize) {
             picked = mod;
@@ -48,6 +54,7 @@ void mods_get_main_mod_name(char* destination, u32 maxSize) {
         }
     }
 
+    if (selectedRomhack) { picked = selectedRomhack; }
     snprintf(destination, maxSize, "%s", picked ? picked->name : "Super Mario 64");
 }
 
