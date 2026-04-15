0e9a1df9536ddae00b5e9d4cc2c8aafca78035fa
EmeraldLockdown <86802223+EmeraldLoc@users.noreply.github.com>
2026-04-14 20:27:50 -0500

    Had to go out of order for these changes, unfortunate

--- src/game/behaviors/elevator.inc.c
+++ src/game/behaviors/elevator.inc.c
@@ -1,13 +1,16 @@
 // elevator.c.inc
 
+static s16 sElevatorHeights[] = { -51, 0,     0, -461, 0,   0, -512, 0,   0,    -2611, 0,
+                     0,   -2360, 0, 0,    214, 0, 0,    -50, 1945, 1,     0 };
+
 void elevator_starting_shake(void) {
     cur_obj_play_sound_2(SOUND_GENERAL_QUIET_POUND1);
     cur_obj_shake_screen(SHAKE_POS_SMALL);
 }
 
 void elevator_act_0(void) {
-    struct MarioState* marioState = nearest_mario_state_to_object(o);
-    struct Object* player = marioState ? marioState->marioObj : NULL;
+    struct MarioState *marioState = nearest_mario_state_to_object(o);
+    struct Object *player = marioState ? marioState->marioObj : NULL;
 
     u8 onPlatform = FALSE;
     for (s32 i = 0; i < MAX_PLAYERS; i++) {
@@ -40,8 +43,8 @@ void elevator_act_0(void) {
 }
 
 void elevator_act_1(void) {
-    struct MarioState* marioState = nearest_mario_state_to_object(o);
-    struct Object* player = marioState ? marioState->marioObj : NULL;
+    struct MarioState *marioState = nearest_mario_state_to_object(o);
+    struct Object *player = marioState ? marioState->marioObj : NULL;
 
     cur_obj_play_sound_1(SOUND_ENV_ELEVATOR1);
     if (o->oTimer == 0 && cur_obj_is_any_player_on_platform()) {
@@ -64,8 +67,8 @@ void elevator_act_1(void) {
 }
 
 void elevator_act_2(void) { // Pretty similar code to action 1
-    struct MarioState* marioState = nearest_mario_state_to_object(o);
-    struct Object* player = marioState ? marioState->marioObj : NULL;
+    struct MarioState *marioState = nearest_mario_state_to_object(o);
+    struct Object *player = marioState ? marioState->marioObj : NULL;
 
     cur_obj_play_sound_1(SOUND_ENV_ELEVATOR1);
     if (o->oTimer == 0 && cur_obj_is_any_player_on_platform()) {
@@ -91,7 +94,7 @@ void elevator_act_2(void) { // Pretty similar code to action 1
 }
 
 void elevator_act_4(void) {
-    struct MarioState* marioState = nearest_mario_state_to_object(o);
+    struct MarioState *marioState = nearest_mario_state_to_object(o);
 
     o->oVelY = 0;
     if (o->oTimer == 0) {
@@ -105,9 +108,8 @@ void elevator_act_4(void) {
     if (marioState && marioState->playerIndex == 0 && o->oAction != 4) { network_send_object(o); }
 }
 
-void elevator_act_3(void) // nearly identical to action 2
-{
-    struct MarioState* marioState = nearest_mario_state_to_object(o);
+void elevator_act_3(void) { // nearly identical to action 2
+    struct MarioState *marioState = nearest_mario_state_to_object(o);
 
     o->oVelY = 0;
     if (o->oTimer == 0) {
@@ -122,15 +124,15 @@ void elevator_act_3(void) // nearly identical to action 2
 }
 
 void bhv_elevator_init(void) {
-    s32 sp1C = BHV_ARR(D_8032F38C, o->oBehParams2ndByte * 3 + 2, s16);
-    if (sp1C == 0) {
-        o->oElevatorUnkF4 = BHV_ARR(D_8032F38C, o->oBehParams2ndByte * 3, s16);
+    s32 index = BHV_ARR(sElevatorHeights, o->oBehParams2ndByte * 3 + 2, s16);
+    if (index == 0) {
+        o->oElevatorUnkF4 = BHV_ARR(sElevatorHeights, o->oBehParams2ndByte * 3, s16);
         o->oElevatorUnkF8 = o->oHomeY;
         o->oElevatorUnkFC = (o->oElevatorUnkF4 + o->oElevatorUnkF8) / 2;
         o->oElevatorUnk100 = cur_obj_has_behavior(bhvRrElevatorPlatform);
     } else {
-        o->oElevatorUnkF4 = BHV_ARR(D_8032F38C, o->oBehParams2ndByte * 3, s16);
-        o->oElevatorUnkF8 = BHV_ARR(D_8032F38C, o->oBehParams2ndByte * 3 + 1, s16);
+        o->oElevatorUnkF4 = BHV_ARR(sElevatorHeights, o->oBehParams2ndByte * 3, s16);
+        o->oElevatorUnkF8 = BHV_ARR(sElevatorHeights, o->oBehParams2ndByte * 3 + 1, s16);
         o->oElevatorUnkFC = (o->oElevatorUnkF4 + o->oElevatorUnkF8) / 2;
         o->oElevatorUnk100 = 2;
     }
@@ -143,15 +145,6 @@ void bhv_elevator_init(void) {
 
 void (*sElevatorActions[])(void) = { elevator_act_0, elevator_act_1, elevator_act_2, elevator_act_3,
                                      elevator_act_4 };
-
-struct SpawnParticlesInfo D_8032F3CC = { 3, 20, MODEL_MIST, 20, 10, 5, 0, 0, 0, 30, 30.0f, 1.5f };
-
-struct SpawnParticlesInfo D_8032F3E0 = { 0, 5, MODEL_SAND_DUST, 0, 0, 20, 20, 0, 252, 30, 5.0f, 2.0f };
-
-s16 D_8032F3F4[] = { 2, -8, 1, 4 };
-
-struct SpawnParticlesInfo D_8032F3FC = { 0,    5,   MODEL_WHITE_PARTICLE_DL, 0, 0, 20, 20, 0, 252, 30,
-                                         2.0f, 2.0f };
 
 void bhv_elevator_loop(void) {
     CUR_OBJ_CALL_ACTION_FUNCTION(sElevatorActions);
