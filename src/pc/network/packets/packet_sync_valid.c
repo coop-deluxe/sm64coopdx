63aef284ad05ecb2caea5887a63f4c135a9a74b6
Emily♥ <77174187+EmilyEmmi@users.noreply.github.com>
2026-05-06 19:54:55 -0400

    Fix erroneous sync valid packets
    When the server received a sync valid packet from another player, it would also set the server player's level. This simple fix makes this no longer happen.

--- src/pc/network/packets/packet_sync_valid.c
+++ src/pc/network/packets/packet_sync_valid.c
@@ -71,7 +71,7 @@ void network_receive_sync_valid(struct Packet* p) {
     }
 
     // inform server
-    if (fromGlobalIndex != gNetworkPlayerServer->globalIndex) {
+    if (gNetworkType != NT_SERVER && fromGlobalIndex != gNetworkPlayerServer->globalIndex) {
         LOG_INFO("informing server of sync valid");
         network_send_sync_valid(gNetworkPlayerServer, courseNum, actNum, levelNum, areaIndex);
     }
