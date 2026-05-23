f396798a5f642c253f952f12795aa686426051e4
Agent X <44549182+AgentXLP@users.noreply.github.com>
2026-05-23 13:45:54 -0400

    Breaklines

--- autogen/lua_definitions/functions.lua
+++ autogen/lua_definitions/functions.lua
@@ -11161,7 +11161,12 @@ function get_shading_fullbright_enabled()
 end
 
 --- @param enabled boolean
---- Enables fullbright mode for shaded materials (`G_LIGHTING`.) If a light color is completely black, the rendered color will default to the shade color. This is for already fullbright materials that set their shade color to something and their light color to black. This visually corrects rendering on materials such as Mario's emblem. Useful for using the lighting engine and having entirely your own shading without the game's own systems and compatibility with most models, not having to used specialized env/prim color approaches for example
+--- Enables fullbright mode for shaded materials (`G_LIGHTING`.)<br>
+--- If a light color is completely black, the rendered color will default to the shade color.<br>
+--- This is for already fullbright materials that set their shade color to something and their light color to black.<br>
+--- This visually corrects rendering on materials such as Mario's emblem.<br>
+--- Useful for using the lighting engine and having entirely your own shading without the game's own systems<br>
+--- and compatibility with most models, not having to used specialized env/prim color approaches for example
 function set_shading_fullbright_enabled(enabled)
     -- ...
 end
