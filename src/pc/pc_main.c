6ba46d8ad7adc2939129a60ee985d5fee3530a91
xLuigiGamerx <88401287+xLuigiGamerx@users.noreply.github.com>
2026-06-22 02:02:20 +0300

    Fix --headless not having a terminal when run from a shortcut

--- src/pc/pc_main.c
+++ src/pc/pc_main.c
@@ -508,7 +508,7 @@ int main(int argc, char *argv[]) {
     // handle Windows console
     DWORD pids[2];
     DWORD pcount = GetConsoleProcessList(pids, 2);
-    if (pcount > 1) {
+    if (pcount > 1 || gCLIOpts.headless) {
         SetConsoleOutputCP(CP_UTF8);
     } else {
         FreeConsole();
