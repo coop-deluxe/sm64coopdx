3355aae7baead10d09c9efefb84c722b44c9edbc
EmeraldLockdown <emeraldlockdown.swifty@gmail.com>
2026-09-16 10:34:31 -0500

    Drop to 1400

--- src/pc/network/packets/packet_download.c
+++ src/pc/network/packets/packet_download.c
@@ -11,7 +11,7 @@
 #include "pc/debuglog.h"
 #include "pc/fs/fmem.h"
 
-#define CHUNK_SIZE 1425 // headers for download packet is 16 bytes for data we send and 8 required bytes for the byte system
+#define CHUNK_SIZE 1400
 #define OFFSET_COUNT 50
 #define CHUNK_GROUP_TIMEOUT 1.5f
 #define GROUP_SIZE (CHUNK_SIZE * OFFSET_COUNT)
