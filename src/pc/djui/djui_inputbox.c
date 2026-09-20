451ae361cc0bf05a292042f43cbdf088f0df60fb
Cooliokid956 <68075390+Cooliokid956@users.noreply.github.com>
2026-09-19 22:51:50 -0500

    add this back in
    okay so I do need this after all okay

--- src/pc/djui/djui_inputbox.c
+++ src/pc/djui/djui_inputbox.c
@@ -346,6 +346,8 @@ void djui_inputbox_on_focus_begin(UNUSED struct DjuiBase* base) {
 }
 
 void djui_inputbox_on_focus_end(UNUSED struct DjuiBase* base) {
+    struct DjuiInputbox *inputbox = (struct DjuiInputbox *) base;
+    inputbox->selection[0] = inputbox->selection[1] = 0;
     gfx_wm_stop_text_input();
 }
 
