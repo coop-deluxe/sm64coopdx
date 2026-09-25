a416aa8b117429a25232f12322fd232024913f2a
Cooliokid956 <68075390+Cooliokid956@users.noreply.github.com>
2026-09-25 02:11:20 -0500

    validate initial value
    also validate bound `value`
    make it `long`
    Reformat the create function to achieve parity with other create functions

--- src/pc/djui/djui_input_number.c
+++ src/pc/djui/djui_input_number.c
@@ -1,14 +1,6 @@
 #include "djui.h"
 #include <errno.h>
 
-static void djui_input_number_render_pre(struct DjuiBase* base, UNUSED bool* unused) {
-    struct DjuiInputNumber *number = (struct DjuiInputNumber*)base;
-    if (*number->value != number->saved) {
-        number->saved = *number->value;
-        snprintf(number->input.buffer, number->input.bufferSize, "%d", number->saved);
-    }
-}
-
 static void djui_input_number_on_text_input(struct DjuiBase *base, char *text) {
     struct DjuiInputNumber *number = (struct DjuiInputNumber*)base;
     u16 *sel = number->input.selection;
@@ -58,7 +50,7 @@ void djui_input_number_text_change(struct DjuiBase *caller) {
 
     input->bufferSize = *text == '-' ? 12 : 11;
 
-    errno = 0; int value = strtol(text, NULL, 10);
+    errno = 0; long value = strtol(text, NULL, 10);
     number->valid =
         !(*text == '\0' || (*text == '-' && text[1] == '\0'))
         && errno != ERANGE && value >= number->min && value <= number->max;
@@ -72,18 +64,27 @@ void djui_input_number_text_change(struct DjuiBase *caller) {
     }
 }
 
+static void djui_input_number_render_pre(struct DjuiBase* base, UNUSED bool* unused) {
+    struct DjuiInputNumber *number = (struct DjuiInputNumber*)base;
+    if (*number->value != number->saved) {
+        number->saved = *number->value;
+        number->input.bufferSize = 12;
+        djui_inputbox_set_number(&number->input, number->saved);
+        djui_input_number_text_change(base);
+    }
+}
+
 struct DjuiInputNumber *djui_input_number_create(struct DjuiBase *parent, int *value, int min, int max) {
-    struct DjuiInputNumber *number = (struct DjuiInputNumber*)djui_inputbox_init(parent, calloc(1, sizeof(struct DjuiInputNumber)), 12);
-    if (*value >= 0) { number->input.bufferSize--; }
-    number->value = value;
-    number->saved = *value;
-    number->min = min;
-    number->max = max;
-    number->valid = true;
-    djui_interactable_hook_text_input(&number->input.base, djui_input_number_on_text_input);
-    djui_interactable_hook_value_change(&number->input.base, djui_input_number_text_change);
-    number->input.base.on_render_pre = djui_input_number_render_pre;
+    struct DjuiInputNumber *number = calloc(1, sizeof(struct DjuiInputNumber));
+    struct DjuiBase *base = &number->input.base;
+    djui_inputbox_init(parent, &number->input, 12);
+    djui_interactable_hook_text_input(base, djui_input_number_on_text_input);
+    djui_interactable_hook_value_change(base, djui_input_number_text_change);
+    base->on_render_pre = djui_input_number_render_pre;
+    number->value = value; number->saved = *value;
+    number->min = min; number->max = max;
     
     djui_inputbox_set_number(&number->input, *value);
+    djui_input_number_text_change(base);
     return number;
 }
