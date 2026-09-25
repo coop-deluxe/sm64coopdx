7352454b559bdee4695257a6760671a1584abfbb
Cooliokid956 <68075390+Cooliokid956@users.noreply.github.com>
2026-09-25 00:29:52 -0500

    yea you right bro mb

--- src/pc/djui/djui_input_number.c
+++ src/pc/djui/djui_input_number.c
@@ -24,7 +24,7 @@ static void djui_input_number_on_text_input(struct DjuiBase *base, char *text) {
         text++;
     } else if (*text == '+') {
         if (*msg == '-') {
-            memmove(msg, msg + 1, strlen(msg) + 1);
+            memmove(msg, msg + 1, strlen(msg));
             if (sel[0] > 0) { sel[0]--; }
             if (sel[1] > 0) { sel[1]--; }
             djui_input_number_text_change(base);
